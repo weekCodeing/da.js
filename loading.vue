3c07ccea010775d8710b9a405d0ebc8733f44e86
魔王哪吒（达达） <59645426+webVueBlog@users.noreply.github.com>
2021-05-19 19:02:43 +0800

    loading
    loading

---
+++ loading.vue
@@ -0,0 +1,27 @@
+<template>
+	<view id="loading">
+		<u-loading :show="show" color="blue" size="40" model="circle"></u-loading>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			show: {
+				type: Boolean,
+				default: true
+			}
+		},
+		data() {
+			return {};
+		},
+		beforeCreate() {},
+		created() {},
+		beforeMount() {},
+		mounted() {},
+		destroyed() {}
+	}
+</script>
+
+<style lang="scss">
+</style>
